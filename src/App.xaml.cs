56b44c7647a7ceed00a92147261b8bb4096ff5e2
almuzahidseyam <almuzahidseyam@gmail.com>
2026-09-14 19:51:21 +0600

    Add crash reporting

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -4,5 +4,23 @@ namespace TrafficSense
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show($"UI Crash: {e.Exception.Message}\n\n{e.Exception.StackTrace}", "TrafficSense Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = e.ExceptionObject as Exception;
+            MessageBox.Show($"Background Crash: {ex?.Message}\n\n{ex?.StackTrace}", "TrafficSense Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
